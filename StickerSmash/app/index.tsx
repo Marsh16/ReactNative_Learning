c1f8eb774353dc923b12a725066b14cca740a8a0
Marsh16 <94427962+Marsh16@users.noreply.github.com>
2025-08-06 14:29:51 +0700

    feat: adding not found screen, and route to about screen

--- StickerSmash/app/index.tsx
+++ StickerSmash/app/index.tsx
@@ -1,9 +1,13 @@
-import { Text, View,  StyleSheet } from 'react-native';
+import { Link } from "expo-router";
+import { Text, View, StyleSheet } from "react-native";
 
 export default function Index() {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Home screen</Text>
+      <Link href="/about">
+        <Text style={styles.button}>Go to about</Text>
+      </Link>
     </View>
   );
 }
@@ -11,11 +15,16 @@ export default function Index() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#25292e',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#25292e",
+    alignItems: "center",
+    justifyContent: "center",
   },
   text: {
-    color: '#fff',
+    color: "#fff",
+  },
+  button: {
+    fontSize: 20,
+    color: "#fff",
+    textDecorationLine: "underline",
   },
 });
