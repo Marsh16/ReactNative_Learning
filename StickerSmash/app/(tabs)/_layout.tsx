91e7d06176817266d624e3368f8ac4ac31e139d7
Marsh16 <94427962+Marsh16@users.noreply.github.com>
2025-08-06 14:47:27 +0700

    feat: adding navigagion tabbar to app, and adding about and home icons, redirect the routes

---
+++ StickerSmash/app/(tabs)/_layout.tsx
@@ -0,0 +1,40 @@
+import { Tabs } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
+
+export default function TabsLayout() {
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: "#ffd33d",
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          headerTitle: "Sticker Smash",
+          tabBarIcon: ({ focused, color }) => (
+            <Ionicons
+              name={focused ? "home-sharp" : "home-outline"}
+              size={30}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="about"
+        options={{
+          headerTitle: "About",
+          tabBarIcon: ({ focused, color }) => (
+            <Ionicons
+              name={focused ? "information-circle-sharp" : "information-circle-outline"}
+              size={30}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen name="+not-found" options={{ headerShown: false }} />
+    </Tabs>
+  );
+}
--- StickerSmash/app/(tabs)/index.tsx
+++ StickerSmash/app/(tabs)/index.tsx
@@ -1,13 +1,9 @@
-import { Link } from "expo-router";
 import { Text, View, StyleSheet } from "react-native";
 
 export default function Index() {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Home screen</Text>
-      <Link href="/about">
-        <Text style={styles.button}>Go to about</Text>
-      </Link>
     </View>
   );
 }
