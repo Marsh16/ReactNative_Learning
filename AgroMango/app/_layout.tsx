05e8a685c83b4d0cbf524c128ade4a7f6883d0a5
Marsh16 <94427962+Marsh16@users.noreply.github.com>
2025-08-08 18:33:11 +0700

    feat: change mb in image welcome, statues bar to dark

--- AgroMango/app/_layout.tsx
+++ AgroMango/app/_layout.tsx
@@ -5,7 +5,7 @@ import { StatusBar } from "expo-status-bar";
 export default function RootLayout() {
   return (
      <>
-    <StatusBar style="light" />
+    <StatusBar style="dark" />
       <Stack>
         <Stack.Screen
           name="index"
--- AgroMango/components/features/welcome-page/WelcomeImage.tsx
+++ AgroMango/components/features/welcome-page/WelcomeImage.tsx
@@ -9,18 +9,15 @@ const PlaceholderImage = require("@/assets/images/welcome-image.png");
 
 export default function WelcomeImage() {
   return (
-    <View style={styles.imageContainer}>
-      <Image source={PlaceholderImage} style={styles.image} />
+    <View className="mb-8 flex-1">
+      <Image source={PlaceholderImage} style={styles.image}/>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  imageContainer: {
-    flex: 1,
-  },
   image: {
-    height: 440,
+    height: 200,
     borderRadius: 18,
   },
 });
